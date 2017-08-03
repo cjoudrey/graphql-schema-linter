b6af92e299473564c1faf1fd3bbf40d44aab1814
Christian Joudrey <cmallette@gmail.com>
2017-08-02 21:31:16 -0700

    Add TypesAreCapitalized rule

---
+++ src/rules/types_are_capitalized.js
@@ -0,0 +1,29 @@
+import { GraphQLError } from 'graphql/error';
+
+export default function(context) {
+  return {
+    ObjectTypeDefinition(node) {
+      const typeName = node.name.value;
+      if (typeName[0] == typeName[0].toLowerCase()) {
+        context.reportError(
+          new GraphQLError(
+            `The object type \`${typeName}\` should start with a capital letter.`,
+            [node],
+          )
+        )
+      }
+    },
+
+    InterfaceTypeDefinition(node) {
+      const typeName = node.name.value;
+      if (typeName[0] == typeName[0].toLowerCase()) {
+        context.reportError(
+          new GraphQLError(
+            `The interface type \`${typeName}\` should start with a capital letter.`,
+            [node],
+          )
+        )
+      }
+    }
+  };
+}
---
+++ test/rules/types_are_capitalized.js
@@ -0,0 +1,57 @@
+import assert from 'assert';
+import { parse } from 'graphql';
+import { visit, visitInParallel } from 'graphql/language/visitor';
+import { validate } from 'graphql/validation';
+import { buildASTSchema } from 'graphql/utilities/buildASTSchema';
+
+import rule from '../../src/rules/types_are_capitalized';
+
+describe('TypesAreCapitalized rule', () => {
+  it('catches object types that are not capitalized', () => {
+    const ast = parse(`
+      type QueryRoot {
+        a: String
+      }
+
+      type a {
+        a: String
+      }
+
+      schema {
+        query: QueryRoot
+      }
+    `);
+
+    const schema = buildASTSchema(ast)
+    const errors = validate(schema, ast, [rule])
+
+    assert.equal(errors.length, 1)
+
+    assert.equal(errors[0].message, 'The object type `a` should start with a capital letter.')
+    assert.deepEqual(errors[0].locations, [{ line: 6, column: 7 }])
+  });
+
+  it('catches interface types that are not capitalized', () => {
+    const ast = parse(`
+      type QueryRoot {
+        a: String
+      }
+
+      interface a {
+        a: String
+      }
+
+      schema {
+        query: QueryRoot
+      }
+    `);
+
+    const schema = buildASTSchema(ast)
+    const errors = validate(schema, ast, [rule])
+
+    assert.equal(errors.length, 1)
+
+    assert.equal(errors[0].message, 'The interface type `a` should start with a capital letter.')
+    assert.deepEqual(errors[0].locations, [{ line: 6, column: 7 }])
+  });
+});
