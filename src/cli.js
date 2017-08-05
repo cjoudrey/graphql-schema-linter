5c182d748bb9c1594265fc61bbda9d880d4c25c2
Christian Joudrey <cmallette@gmail.com>
2017-08-05 00:51:46 -0400

    Handle uncaught exceptions in CLI and exit with code 2 - re: #11

--- src/cli.js
+++ src/cli.js
@@ -2,6 +2,15 @@
 
 import { run } from './runner';
 
+process.on('uncaughtException', (err) => {
+  console.error('It looks like you may have hit a bug in graphql-schema-linter.');
+  console.error('');
+  console.error('It would be super helpful if you could report this here: https://github.com/cjoudrey/graphql-schema-linter/issues/new');
+  console.error('');
+  console.error(err);
+  process.exit(2);
+});
+
 const exitCode = run(process.stdout, process.argv);
 
 process.exit(exitCode);
