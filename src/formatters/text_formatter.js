997c06f6c0dfe9c168828c9268604408249e452e
Christian Joudrey <cmallette@gmail.com>
2017-08-05 20:23:27 -0400

    Add colors to TextFormatter

--- src/formatters/text_formatter.js
+++ src/formatters/text_formatter.js
@@ -1,17 +1,28 @@
+import columnify from 'columnify';
+import figures from 'figures';
+import chalk from 'chalk';
+
 export default function TextFormatter(errors) {
-  const errorsText = errors.map((error) => {
+  const formattedErrors = errors.map((error) => {
     const location = error.locations[0];
 
-    return `${location.line}:${location.column} ${error.message}`;
+    return {
+      location: chalk.dim(`${location.line}:${location.column*4}`),
+      message: error.message,
+      // TODO: Add rule name
+    }
   });
 
-  var summary;
-
-  if (errors.length == 1) {
-    summary = "1 error detected";
-  } else {
-    summary = `${errors.length} errors detected`;
+  if (errors.length == 0) {
+    return chalk.green(`${figures.tick} 0 errors detected\n`);
   }
 
-  return errorsText.join("\n") + "\n" + summary + "\n";
+  const summary = chalk.red(`${figures.cross} ${errors.length} error` +
+    (errors.length > 1 ? 's' : '') + ' detected');
+
+  const errorsText = columnify(formattedErrors, {
+    showHeaders: false,
+  })
+
+  return errorsText + "\n\n" + summary + "\n";
 }
