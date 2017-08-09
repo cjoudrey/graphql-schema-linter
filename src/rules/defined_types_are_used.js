c0ca3ec63597c5de03c662e973212d69a972fb10
Christian Joudrey <cmallette@gmail.com>
2017-08-09 17:49:00 -0400

    Add defined-types-are-used rule

---
+++ src/rules/defined_types_are_used.js
@@ -0,0 +1,42 @@
+import { GraphQLError } from 'graphql/error';
+
+export function DefinedTypesAreUsed(context) {
+  var ignoredTypes = ['Query', 'Mutatation', 'Subscription'];
+  var definedTypes = [];
+  var referencedTypes = new Set();
+
+  var recordDefinedType = node => {
+    if (ignoredTypes.indexOf(node.name.value) == -1) {
+      definedTypes.push(node);
+    }
+  };
+
+  return {
+    ScalarTypeDefinition: recordDefinedType,
+    ObjectTypeDefinition: recordDefinedType,
+    InterfaceTypeDefinition: recordDefinedType,
+    UnionTypeDefinition: recordDefinedType,
+    EnumTypeDefinition: recordDefinedType,
+    InputObjectTypeDefinition: recordDefinedType,
+
+    NamedType: node => {
+      referencedTypes.add(node.name.value);
+    },
+
+    Document: {
+      leave: node => {
+        definedTypes.forEach(node => {
+          if (!referencedTypes.has(node.name.value)) {
+            context.reportError(
+              new GraphQLError(
+                `The type \`${node.name.value}\` is defined in the ` +
+                  `schema but not used anywhere.`,
+                [node]
+              )
+            );
+          }
+        });
+      },
+    },
+  };
+}
