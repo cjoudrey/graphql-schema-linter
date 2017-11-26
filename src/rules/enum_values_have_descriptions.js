f0c46b4b5e5544d5ad024fe893b5cc6572375891
Christian Joudrey <cmallette@gmail.com>
2017-11-26 18:11:23 -0500

    Use ValidationError instead of GraphQLError in EnumValuesHaveDescriptions

--- src/rules/enum_values_have_descriptions.js
+++ src/rules/enum_values_have_descriptions.js
@@ -1,5 +1,5 @@
 import { getDescription } from 'graphql/utilities/buildASTSchema';
-import { GraphQLError } from 'graphql/error';
+import { ValidationError } from '../validation_error';
 
 export function EnumValuesHaveDescriptions(context) {
   return {
@@ -12,7 +12,8 @@ export function EnumValuesHaveDescriptions(context) {
       const parentName = ancestors[ancestors.length - 1].name.value;
 
       context.reportError(
-        new GraphQLError(
+        new ValidationError(
+          'enum-values-have-descriptions',
           `The enum value \`${parentName}.${enumValue}\` is missing a description.`,
           [node]
         )
