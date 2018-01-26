8672e3917a743c788abeb1ade780a692f9dc00f6
Christian Joudrey <cmallette@gmail.com>
2018-01-26 18:13:12 -0500

    Add test helpers for asserting rules

---
+++ test/assertions.js
@@ -0,0 +1,31 @@
+import assert from 'assert';
+import { parse } from 'graphql';
+import { validate } from 'graphql/validation';
+import { buildASTSchema } from 'graphql/utilities/buildASTSchema';
+import { kebabCase } from 'lodash';
+
+export function expectFailsRule(rule, schemaSDL, expectedErrors = []) {
+  const ast = parse(schemaSDL);
+  const schema = buildASTSchema(ast);
+  const errors = validate(schema, ast, [rule]);
+
+  assert(errors.length > 0, "Expected rule to fail but didn't");
+
+  assert.deepEqual(
+    errors,
+    expectedErrors.map(expectedError => {
+      return Object.assign(expectedError, {
+        ruleName: kebabCase(rule.name),
+        path: undefined,
+      });
+    })
+  );
+}
+
+export function expectPassesRule(rule, schemaSDL, expectedErrors = []) {
+  const ast = parse(schemaSDL);
+  const schema = buildASTSchema(ast);
+  const errors = validate(schema, ast, [rule]);
+
+  assert(errors.length == 0, "Expected rule to pass but didn't");
+}
