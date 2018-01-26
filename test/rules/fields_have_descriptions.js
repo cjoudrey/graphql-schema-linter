960c0f871e387c9d623606314c663b2e66259830
Christian Joudrey <cmallette@gmail.com>
2018-01-26 18:13:56 -0500

    Use test helpers in FieldsHaveDescriptions tests

--- test/rules/fields_have_descriptions.js
+++ test/rules/fields_have_descriptions.js
@@ -1,13 +1,11 @@
-import assert from 'assert';
-import { parse } from 'graphql';
-import { validate } from 'graphql/validation';
-import { buildASTSchema } from 'graphql/utilities/buildASTSchema';
-
 import { FieldsHaveDescriptions } from '../../src/rules/fields_have_descriptions';
+import { expectFailsRule } from '../assertions';
 
 describe('FieldsHaveDescriptions rule', () => {
   it('catches fields that have no description', () => {
-    const ast = parse(`
+    expectFailsRule(
+      FieldsHaveDescriptions,
+      `
       type QueryRoot {
         withoutDescription: String
         withoutDescriptionAgain: String!
@@ -19,25 +17,19 @@ describe('FieldsHaveDescriptions rule', () => {
       schema {
         query: QueryRoot
       }
-    `);
-
-    const schema = buildASTSchema(ast);
-    const errors = validate(schema, ast, [FieldsHaveDescriptions]);
-
-    assert.equal(errors.length, 2);
-
-    assert.equal(errors[0].ruleName, 'fields-have-descriptions');
-    assert.equal(
-      errors[0].message,
-      'The field `QueryRoot.withoutDescription` is missing a description.'
+    `,
+      [
+        {
+          message:
+            'The field `QueryRoot.withoutDescription` is missing a description.',
+          locations: [{ line: 3, column: 9 }],
+        },
+        {
+          message:
+            'The field `QueryRoot.withoutDescriptionAgain` is missing a description.',
+          locations: [{ line: 4, column: 9 }],
+        },
+      ]
     );
-    assert.deepEqual(errors[0].locations, [{ line: 3, column: 9 }]);
-
-    assert.equal(errors[1].ruleName, 'fields-have-descriptions');
-    assert.equal(
-      errors[1].message,
-      'The field `QueryRoot.withoutDescriptionAgain` is missing a description.'
-    );
-    assert.deepEqual(errors[1].locations, [{ line: 4, column: 9 }]);
   });
 });
