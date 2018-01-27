41980504b8339e8251fae3255187077888dc648d
Christian Joudrey <cmallette@gmail.com>
2018-01-26 19:27:15 -0500

    Use test helpers in InputObjectValuesAreCamelCased tests

--- test/rules/input_object_values_are_camel_cased.js
+++ test/rules/input_object_values_are_camel_cased.js
@@ -1,13 +1,11 @@
-import assert from 'assert';
-import { parse } from 'graphql';
-import { validate } from 'graphql/validation';
-import { buildASTSchema } from 'graphql/utilities/buildASTSchema';
-
 import { InputObjectValuesAreCamelCased } from '../../src/rules/input_object_values_are_camel_cased';
+import { expectFailsRule, expectPassesRule } from '../assertions';
 
 describe('InputObjectValuesAreCamelCased rule', () => {
   it('catches input object type values that are not camel cased', () => {
-    const ast = parse(`
+    expectFailsRule(
+      InputObjectValuesAreCamelCased,
+      `
       type Query {
         hello: String
       }
@@ -17,38 +15,31 @@ describe('InputObjectValuesAreCamelCased rule', () => {
 
         withDescription: String
       }
-    `);
-
-    const schema = buildASTSchema(ast);
-    const errors = validate(schema, ast, [InputObjectValuesAreCamelCased]);
-
-    assert.equal(errors.length, 1);
-
-    assert.equal(errors[0].ruleName, 'input-object-values-are-camel-cased');
-    assert.equal(
-      errors[0].message,
-      'The input value `User.user_name` is not camel cased.'
+    `,
+      [
+        {
+          message: 'The input value `User.user_name` is not camel cased.',
+          locations: [{ line: 7, column: 9 }],
+        },
+      ]
     );
-    assert.deepEqual(errors[0].locations, [{ line: 7, column: 9 }]);
   });
 
   it('catches arguments that are not camel cased', () => {
-    const ast = parse(`
+    expectFailsRule(
+      InputObjectValuesAreCamelCased,
+      `
       type Query {
         hello(argument_without_description: String): String
       }
-    `);
-
-    const schema = buildASTSchema(ast);
-    const errors = validate(schema, ast, [InputObjectValuesAreCamelCased]);
-
-    assert.equal(errors.length, 1);
-
-    assert.equal(errors[0].ruleName, 'input-object-values-are-camel-cased');
-    assert.equal(
-      errors[0].message,
-      'The input value `hello.argument_without_description` is not camel cased.'
+    `,
+      [
+        {
+          message:
+            'The input value `hello.argument_without_description` is not camel cased.',
+          locations: [{ line: 3, column: 15 }],
+        },
+      ]
     );
-    assert.deepEqual(errors[0].locations, [{ line: 3, column: 15 }]);
   });
 });
