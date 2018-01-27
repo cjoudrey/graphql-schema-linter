774e7b584f72bcb4e187138cdc5a9ea16e34804e
Christian Joudrey <cmallette@gmail.com>
2018-01-26 19:32:47 -0500

    Use test helpers in TypesAreCapitalized tests

--- test/rules/types_are_capitalized.js
+++ test/rules/types_are_capitalized.js
@@ -1,14 +1,11 @@
-import assert from 'assert';
-import { parse } from 'graphql';
-import { visit, visitInParallel } from 'graphql/language/visitor';
-import { validate } from 'graphql/validation';
-import { buildASTSchema } from 'graphql/utilities/buildASTSchema';
-
 import { TypesAreCapitalized } from '../../src/rules/types_are_capitalized';
+import { expectFailsRule, expectPassesRule } from '../assertions';
 
 describe('TypesAreCapitalized rule', () => {
   it('catches object types that are not capitalized', () => {
-    const ast = parse(`
+    expectFailsRule(
+      TypesAreCapitalized,
+      `
       type QueryRoot {
         a: String
       }
@@ -20,23 +17,20 @@ describe('TypesAreCapitalized rule', () => {
       schema {
         query: QueryRoot
       }
-    `);
-
-    const schema = buildASTSchema(ast);
-    const errors = validate(schema, ast, [TypesAreCapitalized]);
-
-    assert.equal(errors.length, 1);
-
-    assert.equal(errors[0].ruleName, 'types-are-capitalized');
-    assert.equal(
-      errors[0].message,
-      'The object type `a` should start with a capital letter.'
+    `,
+      [
+        {
+          message: 'The object type `a` should start with a capital letter.',
+          locations: [{ line: 6, column: 12 }],
+        },
+      ]
     );
-    assert.deepEqual(errors[0].locations, [{ line: 6, column: 12 }]);
   });
 
   it('catches interface types that are not capitalized', () => {
-    const ast = parse(`
+    expectFailsRule(
+      TypesAreCapitalized,
+      `
       type QueryRoot {
         a: String
       }
@@ -48,18 +42,13 @@ describe('TypesAreCapitalized rule', () => {
       schema {
         query: QueryRoot
       }
-    `);
-
-    const schema = buildASTSchema(ast);
-    const errors = validate(schema, ast, [TypesAreCapitalized]);
-
-    assert.equal(errors.length, 1);
-
-    assert.equal(errors[0].ruleName, 'types-are-capitalized');
-    assert.equal(
-      errors[0].message,
-      'The interface type `a` should start with a capital letter.'
+    `,
+      [
+        {
+          message: 'The interface type `a` should start with a capital letter.',
+          locations: [{ line: 6, column: 17 }],
+        },
+      ]
     );
-    assert.deepEqual(errors[0].locations, [{ line: 6, column: 17 }]);
   });
 });
