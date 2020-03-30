5a038b9c405d452fea5a08d19c87bd0c1bbfae5c
Eric Dobbertin <eric@dairystatedesigns.com>
2020-03-30 14:50:30 -0500

    feat: add input-object-fields-sorted-alphabetically built-in rule

---
+++ src/rules/input_object_fields_sorted_alphabetically.js
@@ -0,0 +1,22 @@
+import { ValidationError } from '../validation_error';
+import listIsAlphabetical from '../util/listIsAlphabetical';
+
+export function InputObjectFieldsSortedAlphabetically(context) {
+  return {
+    InputObjectTypeDefinition(node) {
+      const fieldList = (node.fields || []).map(field => field.name.value);
+      const { isSorted, sortedList } = listIsAlphabetical(fieldList);
+
+      if (!isSorted) {
+        context.reportError(
+          new ValidationError(
+            'input-object-fields-sorted-alphabetically',
+            `The fields of input type \`${node.name.value}\` should be sorted alphabetically. ` +
+              `Expected sorting: ${sortedList.join(', ')}`,
+            [node]
+          )
+        );
+      }
+    },
+  };
+}
---
+++ test/rules/input_object_fields_sorted_alphabetically.js
@@ -0,0 +1,35 @@
+import { InputObjectFieldsSortedAlphabetically } from '../../src/rules/input_object_fields_sorted_alphabetically';
+import { expectFailsRule, expectPassesRule } from '../assertions';
+
+describe('InputObjectFieldsSortedAlphabetically rule', () => {
+  it('catches enums that are not sorted alphabetically', () => {
+    expectFailsRule(
+      InputObjectFieldsSortedAlphabetically,
+      `
+      input Stage {
+        foo: String
+        bar: String
+      }
+    `,
+      [
+        {
+          message:
+            'The fields of input type `Stage` should be sorted alphabetically. Expected sorting: bar, foo',
+          locations: [{ line: 2, column: 7 }],
+        },
+      ]
+    );
+  });
+
+  it('allows enums that are sorted alphabetically ', () => {
+    expectPassesRule(
+      InputObjectFieldsSortedAlphabetically,
+      `
+      input Stage {
+        bar: String
+        foo: String
+      }
+    `
+    );
+  });
+});
