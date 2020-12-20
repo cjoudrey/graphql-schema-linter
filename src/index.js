0cd0f42ce3276d48d13857a4efc405ff653a30e8
vvakame <vvakame+dev@gmail.com>
2020-12-20 15:40:07 +0900

    Export ValidationError from index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 module.exports.runner = require('./runner.js');
 module.exports.validator = require('./validator.js');
 module.exports.configuration = require('./configuration.js');
+module.exports.ValidationError = require('./validation_error.js').ValidationError;
